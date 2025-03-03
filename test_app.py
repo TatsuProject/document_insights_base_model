e543401fa7d23dde769c2f0d52e406d60a623d80
atkinson-ai <beyondblocs@gmail.com>
2025-03-03 18:53:23 +0500

    doc-classifier added

--- test_app.py
+++ test_app.py
@@ -9,14 +9,14 @@ with open("", "rb") as image_file: # give your image path here
     encoded_string = base64.b64encode(image_file.read()).decode()
 
 # Prepare the request payload
-payload = {'image': encoded_string, "request_id": str(uuid.uuid4())}
+payload = {'image': encoded_string, "request_id": str(uuid.uuid4()), "task_type": "checkbox"}
 
 # Function to send a POST request
 def send_request():
     try:
         response = requests.post('http://127.0.0.1:5000/predict', json=payload)
         print(f"Status Code: {response.status_code}")
-        predictions = response.json().get("predictions", [])
+        predictions = response.json()
         print(f"Response: {predictions}")
     except Exception as e:
         print(f"Request failed: {e}")
